a1e3f73aebcc2588db91208da92e081010841cb8
Andrew Sadowski <andrew.n.sadowski@gmail.com>
2018-02-17 17:57:25 -0800

    add get route

--- src/server.js
+++ src/server.js
@@ -6,4 +6,8 @@ import { signin, protect } from './api/modules/auth'
 // Declare an app from express
 const app = express();
 
+app.get('/', (req, res) => {
+  res.json({ok: true})
+})
+
 export default app
