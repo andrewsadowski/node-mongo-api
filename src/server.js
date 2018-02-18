fd4d3486e1ae665e73b0d7909c75ba2888b35733
Andrew Sadowski <andrew.n.sadowski@gmail.com>
2018-02-17 17:45:35 -0800

    updated src and test files

--- src/server.js
+++ src/server.js
@@ -1,23 +1,9 @@
 import express from 'express'
 import setupMiddware from './middleware'
-import { restRouter, graphQLRouter } from './api'
+import { restRouter } from './api'
 import { connect } from './db'
 import { signin, protect } from './api/modules/auth'
-import { graphiqlExpress } from 'apollo-server-express'
 // Declare an app from express
-const app = express()
-
-setupMiddware(app)
-connect()
-// setup basic routing for index route
-
-app.use('/signin', signin)
-app.use('/api', protect, restRouter)
-app.use('/graphql',  graphQLRouter)
-app.use('/docs', graphiqlExpress({ endpointURL: '/graphql' }))
-// catch all
-app.all('*', (req, res) => {
-  res.json({ok: true})
-})
+const app = null
 
 export default app
