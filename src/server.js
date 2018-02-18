ed1251a31946c1ab522e0ccf923082b689683737
Andrew Sadowski <andrew.n.sadowski@gmail.com>
2018-02-17 17:46:35 -0800

    updated express instantiation

--- src/server.js
+++ src/server.js
@@ -4,6 +4,6 @@ import { restRouter } from './api'
 import { connect } from './db'
 import { signin, protect } from './api/modules/auth'
 // Declare an app from express
-const app = null
+const app = express();
 
 export default app
