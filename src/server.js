79ed61d2e0c631ac3e37e3d0c01ddc86ad8a68f8
Andrew Sadowski <andrew.n.sadowski@gmail.com>
2018-02-17 21:07:18 -0800

    mounted restRouter to app

--- src/server.js
+++ src/server.js
@@ -6,7 +6,13 @@ import { signin, protect } from './api/modules/auth'
 // Declare an app from express
 const app = express();
 
-app.get('/', (req, res) => {
+setupMiddware(app)
+// connect()
+
+app.use('/signin', signin)
+app.use('/api', restRouter)
+
+app.all('*', (req, res) => {
   res.json({ok: true})
 })
 
