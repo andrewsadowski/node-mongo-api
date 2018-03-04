3863e33337d9ee5095dcca828daf7c879e37171d
Andrew Sadowski <andrew.n.sadowski@gmail.com>
2018-03-03 16:44:26 -0800

    added createOne controller

--- src/api/modules/query.js
+++ src/api/modules/query.js
@@ -1,57 +1,49 @@
-import merge from 'lodash.merge'
-const testData = {message: 'hello'}
+import merge from 'lodash.merge';
+const testData = { message: 'hello' };
 
 // These are generic methods used in the generic controllers for all models
 export const controllers = {
   createOne(model, body) {
-    return Promise.resolve(testData)
+    return Promise.resolve(testData);
   },
 
   updateOne(docToUpdate, update) {
-    return Promise.resolve(testData)
+    return Promise.resolve(testData);
   },
 
   deleteOne(docToDelete) {
-    return Promise.resolve(testData)
+    return Promise.resolve(testData);
   },
 
   getOne(docToGet) {
-    return Promise.resolve(testData)
+    return Promise.resolve(testData);
   },
 
   getAll(model) {
-    return Promise.resolve(testData)
+    return Promise.resolve(testData);
   },
 
   findByParam(model, id) {
-    return Promise.resolve(testData)
+    return Promise.resolve(testData);
   }
-}
+};
 
-export const createOne = (model) => (req, res, next) => {
+export const createOne = model => (req, res, next) => {
+  return controllers
+    .createOne(model, req.body)
+    .then(result => res.json(res))
+    .catch(e => res.status(500).send('Things are not looking good'));
+};
 
-}
+export const updateOne = model => async (req, res, next) => {};
 
-export const updateOne = (model) => async (req, res, next) => {
+export const deleteOne = model => (req, res, next) => {};
 
-}
+export const getOne = model => (req, res, next) => {};
 
-export const deleteOne = (model) => (req, res, next) => {
+export const getAll = model => (req, res, next) => {};
 
-}
-
-export const getOne = (model) => (req, res, next) => {
-
-}
-
-export const getAll = (model) => (req, res, next) => {
-
-}
-
-export const findByParam = (model) => (req, res, next, id) => {
-  
-}
-
+export const findByParam = model => (req, res, next, id) => {};
 
 export const generateControllers = (model, overrides = {}) => {
   const defaults = {
@@ -61,7 +53,7 @@ export const generateControllers = (model, overrides = {}) => {
     deleteOne: deleteOne(model),
     updateOne: updateOne(model),
     createOne: createOne(model)
-  }
+  };
 
-  return {...defaults, ...overrides}
-}
+  return { ...defaults, ...overrides };
+};
